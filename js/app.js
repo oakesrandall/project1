4032933c890d37f7fb7c1fec19bb40bd39758927
David_Randall <oakesrandall@gmail.com>
2017-07-17 07:38:46 -0600

    commented js file, cleaned up code

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(function() {
   console.log("Page is ready");
 });
 
+//creates a variable to hold high scores that will survive a browser refresh
 function createStorage() {
   localStorage.setItem("currentPlayer", 2);
   localStorage.setItem("streak1", 0);
@@ -28,11 +29,13 @@ var player = {
 };
 
 
-
+//counter variables used in later functions
 var countSequences = 0;
-
 var counter = 0;
 
+
+//function that runs when the play game button is pressed.  Runs the functions
+// to get the game started
 function playGame() {
    console.log("STARTED NEW GAME");
    resetBoard();
@@ -50,22 +53,32 @@ function generateRandomNumber() {
 
 //allows the board to be clicked and gives me the number of the
 //block that was clicked
+
 function userClick() {
   console.log("Ran userClick. Waiting for click");
   $(".clickMe").on("click", function() {
-    computer.testElement = computer.flashedItems.shift();
+//flashes the color of the block that was clicked
     $(this).animate({opacity: 0.1}, 300).animate({opacity: 1}, 200);
     console.log("Click registered");
     var tempVar = $(this).attr("id");
     player.response = (tempVar[5]);
+//plays the sound of the block that was clicked
     $("#" + "audio" + player.response)[0].play();
+//waits for the sound to play, then checks if right button was clicked
     setTimeout(testCase, 400);
     });
 }
 
+//pulls the first element out of the computer's array and
+//checks to see if if matches the player's input
+//if it does, it puts it into another array
 function testCase() {
+    computer.testElement = computer.flashedItems.shift();
     if (computer.testElement == player.response) {
       computer.sequence.push(computer.testElement);
+//if the computer's array is empty, level up and turn off clicks while
+//the computer builds a new sequence and takes its turn flashing it on
+//the board
       if (computer.flashedItems.length === 0){
          levelUp();
          noClick();
@@ -73,32 +86,39 @@ function testCase() {
          setTimeout(flashArray, 1000);
       }
     } else {
+//if it doesn't match, it runs the youLose function after a brief delay
+//to allow the colors and sounds to finish playing
     setTimeout(youLose, 500);
   }
 }
 
 
 
-
+//turns off the ability to click the board (to be used while the computer is playing)
 function noClick() {
   $(".clickMe").off("click");
   console.log("Ran noClick");
 }
 
+//stretch goal, not implemented yet. currently game goes on forever
 function youWin(){
   alert("Wow! You made it through 20 levels!");
 }
 
+//alerts player that they have lost and plays awful sound.  reloads page.
 function youLose() {
   $("#buzz")[0].play();
   if(!alert("You lose!")){window.location.reload();}
 }
 
+//increases the player's level and changes the player's highest level to their current
+//streak, assuming their current score is higher than their past scores.
 function levelUp() {
   player.level ++;
   if (player.level > localStorage["streak" + localStorage.currentPlayer]) {
     localStorage["streak" + localStorage.currentPlayer] = player.level;
   }
+//resets variables and counters to get ready for the next round
   countSequences = 0;
   counter = 0;
   player.response = 0;
@@ -110,7 +130,7 @@ function levelUp() {
   console.log("Went up a level. Now at level " + player.level);
 }
 
-//resets all player stats
+//resets all player stats stored in localStorage
 function resetPlayerStats() {
   localStorage.streak1 = 0;
   localStorage.streak2 = 0;
@@ -131,20 +151,15 @@ function resetBoard() {
   switchPlayer();
 }
 
-//resets player response variable
-function resetPlayerResponse() {
-  player.response = 0;
-  console.log("Reset player.response.  It is now " + player.response);
-}
-
-//creates an array of randomly generated numbers that is one element longer than
-//the number of times the player has successfully matched the computer
+//pushes the randomly generated number into the computer's array
 function createComputerSequence() {
   console.log("ran createComputerSequence");
   computer.sequence.push(generateRandomNumber());
   console.log("Pushed random number into computer.sequence " + computer.sequence[computer.sequence.length -1]);
 }
 
+//determines how many randomly generated numbers should be added into the
+//computer's sequence based on how high a player's level is
 function createMultipleSequences() {
   console.log("Ran createMultipleSequences");
   if (countSequences < player.level) {
@@ -156,6 +171,7 @@ function createMultipleSequences() {
   computer.arrayLength = computer.sequence.length;
 }
 
+//switches players
 function switchPlayer() {
   console.log("Switching player");
   if (localStorage.currentPlayer === "1") {
@@ -168,13 +184,12 @@ function switchPlayer() {
 //Takes the computer's randomly generated sequence and outputs a number used
 //to make a specific section flash.  This function has a time delay built in so
 //the blocks don't flash too quickly
-
 var counter = 0;
 function flashArray(){
     flashColor();
     if (counter < computer.arrayLength -1) {
       counter++;
-      setTimeout(flashArray, 600);
+      setTimeout(flashArray, 615);
     }
 }
 
@@ -197,15 +212,18 @@ $("#block2").corner("bite, bl 60px");
 $("#block3").corner("bite, tr 60px");
 $("#block4").corner("bite, tl 60px");
 
+//listens for clicks on the reset player stats button and runs resetPlayerStats
 $("button#resetButton").click(function() {
   console.log("clicked reset stats");
   resetPlayerStats();
   if(!alert("Player Stats have been reset")){window.location.reload();}
 });
 
+//listens for clicks on the start game button and starts the game
 $("button#startButton").click(function() {
   setTimeout(playGame, 600);
 });
 
+//displays the longest streaks retrieved from local storage for each player
 $("#playerOneScore").text("Player One Longest Streak: " + localStorage.streak1);
 $("#playerTwoScore").text("Player Two Longest Streak: " + localStorage.streak2);
